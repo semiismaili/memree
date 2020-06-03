f5077cc5eb6aaf2e0495a1413c9f2ea0aafe09b7
Semi Ismaili <semi-97@hotmail.com>
2020-06-03 14:08:30 -0700

    Set up ViewController.swift as the delegate and datasource of the ControllerView

--- memree/ViewController.swift
+++ memree/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var model = CardModel()
     var cardArray = [Card]()
     
@@ -18,8 +20,28 @@ class ViewController: UIViewController {
         
         cardArray = model.getCards()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
-
+    
+    //MARK: ~ UICollectionView Protocol Methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+            return cell
+       }
+       
+    //gets called when the user taps on a cell
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
 
 }
 
